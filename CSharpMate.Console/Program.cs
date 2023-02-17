535a57b23ee3ec313a1a2763d3eaf0c9b92fd06a
Reza <rbasereh@gmail.com>
2023-02-17 17:48:05 +0330

    add CSharpMate.Core add CSharpMate.Console

---
+++ CSharpMate.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CSharpMate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
