a81a6ee3c62e00f01eb47578c388a3f4dc6f0dc9
Reza <rbasereh@gmail.com>
2023-02-17 13:40:54 +0330

    Add project files.

---
+++ CSharpMate.CommandLine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpMate.CommandLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
